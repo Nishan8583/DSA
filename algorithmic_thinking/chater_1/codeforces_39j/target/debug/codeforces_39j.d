1f7f1632400667570b426a551b2c28077fd02a95
nishan8583 <nishanmaharjan17@gmail.com>
2024-09-28 14:04:58 -0700

    removing useless files

--- algorithmic_thinking/chater_1/codeforces_39j/target/debug/codeforces_39j.d
+++
@@ -1,1 +0,0 @@
-/home/nihan/projects/DSA/algorithmic_thinking/chater_1/codeforces_39j/target/debug/codeforces_39j: /home/nihan/projects/DSA/algorithmic_thinking/chater_1/codeforces_39j/src/main.rs
--- algorithmic_thinking/chater_1/codeforces_39j/target/debug/deps/codeforces_39j-09cd7027b74c5e96.d
+++
@@ -1,5 +0,0 @@
-/home/nihan/projects/DSA/algorithmic_thinking/chater_1/codeforces_39j/target/debug/deps/codeforces_39j-09cd7027b74c5e96: src/main.rs
-
-/home/nihan/projects/DSA/algorithmic_thinking/chater_1/codeforces_39j/target/debug/deps/codeforces_39j-09cd7027b74c5e96.d: src/main.rs
-
-src/main.rs:
